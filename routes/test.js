2ba33956d5ef312b996588c309d8821bcbe09a46
peter lau <cwipac@connect.ust.hk>
2018-08-25 09:29:28 +0800

    first try on db

--- routes/test.js
+++ routes/test.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
 const passport = require('../middleware/session/passport-local');
 
+
+
+const MongoClient = require('mongodb').MongoClient;
+ 
+// Connection URL
+const url = 'mongodb://localhost:27017';
+ 
+// Database Name
+const dbName = 'myproject';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  console.log("Connected successfully to server");
+  var dbo = client.db(dbName);
+  dbo.createCollection('customers', function(err, res){
+  	console.log('collection is created');
+  });
+  var myobj = { name: "Company Inc", address: "Highway 37" };
+  dbo.collection("customers").insertOne(myobj);
+
+  dbo.collection("customers").findOne({}, function(err,result){
+  	console.log(result);
+  });
+  client.close();
+});
+
 router.get('/test', function(req,res){
 	res.sendfile('test.html', {root: __dirname+'/../views'});
 });
